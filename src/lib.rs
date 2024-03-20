bc45f44dea096beba0498b5d440baafe7de2acba
orangekiwi-io <hello@orangekiwi.io>
2024-03-20 17:11:10 +0000

    Create new instance of PDFComposer development

--- src/lib.rs
+++ src/lib.rs
@@ -1,15 +1,52 @@
-pub fn add(left: usize, right: usize) -> usize {
-    println!("PDF Composer: Add");
-    left + right
+// Copyright © 2024 PDF OK (pdf_ok). All rights reserved.
+// SPDX-License-Identifier: Apache-2.0 OR MIT
+
+// Crate configuration
+#![crate_name = "pdf_composer"]
+#![crate_type = "lib"]
+
+use std::path::PathBuf;
+
+// PDFComposer struct
+pub struct PDFComposer {
+    // Add Front Matter (YAML) markdown files directly
+    pub fmy_source_files: Vec<PathBuf>,
 }
 
-#[cfg(test)]
-mod tests {
-    use super::*;
-
-    #[test]
-    fn it_works() {
-        let result = add(2, 2);
-        assert_eq!(result, 4);
+impl PDFComposer {
+    /// Constructor function to create a new instance of PDFComposer
+    pub fn new() -> Self {
+        println!("PDF Composer new!");
+        // Create and return a new instance of PDFComposer
+        PDFComposer {
+            fmy_source_files: Vec::new(),
+        }
     }
+
+    /// Method to add Front Matter (YAML) markdown files programmatically, one by one
+    pub fn add_path(&mut self, path: PathBuf) {
+        self.fmy_source_files.push(path);
+    }
+
+    // TODO RL Remove later. Debug dev
+    // Method to print all paths in the vector
+    pub fn print_paths(&self) {
+        for path in &self.fmy_source_files {
+            println!("{:?}", path);
+        }
+    }
+}
+
+impl Default for PDFComposer {
+    // Add a default implementation that creates an instance of PDFComposer with default values
+    fn default() -> Self {
+        // You can define default values for fields here
+        // For demonstration purposes, let's assume all fields are set to default values
+        PDFComposer::new()
+    }
+}
+
+// Publicly expose the new function for creating instances of PDFComposer
+pub mod pdf_composer {
+    pub use super::PDFComposer;
 }
