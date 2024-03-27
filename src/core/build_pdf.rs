16cb15bfecdbcfb4331b7a4c2b39e0adfb2ab938
orangekiwi-io <hello@orangekiwi.io>
2024-03-27 15:30:49 +0000

    Switched out headless_chrome for chromiumoxide because of async/await support (headless, lack of).

--- src/core/build_pdf.rs
+++ src/core/build_pdf.rs
@@ -1,5 +1,4 @@
 use colored::Colorize;
-use headless_chrome::{Browser, LaunchOptions};
 use lopdf::{Document, Object as LopdfObject, StringFormat};
 use serde_yaml::Value;
 use std::collections::BTreeMap;
@@ -7,6 +6,9 @@ use std::fs;
 use std::path::{Path, PathBuf};
 
 use crate::utils::extract_to_end_string;
+use async_std::task;
+use chromiumoxide::{cdp::browser_protocol::page::PrintToPdfParams, Browser, BrowserConfig};
+use futures::StreamExt;
 
 /// Generates a PDF from HTML content using headless Chrome.
 ///
@@ -39,130 +41,141 @@ pub fn build_pdf(
     dictionary_entries: BTreeMap<String, String>,
     pdf_version: String,
 ) -> Result<(), Box<dyn std::error::Error>> {
-    // println!("{}: {:#?}", "dictionary_entries".cyan(), &dictionary_entries);
-    // Remove the markdown, md, file extension
-    let filename_path = source_file.trim_end_matches(".md");
-    // Extract only the file name
-    let extracted_filename = extract_to_end_string(filename_path, '/');
+    task::block_on(async {
+        // println!("{}: {:#?}", "dictionary_entries".cyan(), &dictionary_entries);
+        // Remove the markdown, md, file extension
+        let filename_path = source_file.trim_end_matches(".md");
+        // Extract only the file name
+        let extracted_filename = extract_to_end_string(filename_path, '/');
 
-    let mut string_values_btreemap: BTreeMap<String, String> = BTreeMap::new();
-    for (key, value) in yaml_btreemap {
-        if let Value::String(string_value) = value {
-            string_values_btreemap.insert(key, string_value);
-        }
-    }
-
-    let browser = Browser::new(LaunchOptions::default())?; // Start a new headless Chrome browser instance
-    let tab = browser.new_tab()?; // Open a new tab
-
-    let mut html = String::new();
-    // Encode the HTML content to URL-safe format
-    // url_escape:: comes from the url_escape crate
-    url_escape::encode_query_to_string(generated_html, &mut html);
-
-    fs::create_dir_all(&output_directory)?;
-    let mut pdf_file = extracted_filename.unwrap().to_string();
-    pdf_file.push_str(".pdf");
-
-    let pdf_file_path = Path::new(&output_directory).join(pdf_file);
-
-    // Navigate the tab to the HTML content.
-    // In this case, the page is a data stream
-    tab.navigate_to(format!("data:text/html;charset=utf-8,{}", html).as_str())?;
-
-    // Convert the page to PDF format
-    let pdf = tab.print_to_pdf(None)?;
-
-    // Create a new PDF document
-    let mut doc: Document = Document::load_mem(&pdf)?;
-    doc.version = pdf_version;
-
-    #[allow(unused_variables)]
-    let mut object_count: i32 = 0;
-    // Iterate over the objects in the PDF document and count them
-    for object_element in &mut doc.objects {
-        let (_key, object) = object_element;
-        match object {
-            LopdfObject::Dictionary(dictionary) => {
-                // Variable to track if Creator key is present
-                let mut creator_found = false;
-
-                // Print out the dictionary entries
-                for (key, value) in dictionary.iter_mut() {
-                    let ascii_key = String::from_utf8_lossy(key);
-
-                    // Iterate over the key-value pairs in the dictionary
-                    // Check if the key is "Creator"
-                    if ascii_key == "Creator" {
-                        // Update the value associated with the key
-                        let ascii_string = string_values_btreemap.get("generator").unwrap();
-                        let ascii_bytes: Vec<u8> = ascii_string.as_bytes().to_vec();
-                        *value = lopdf::Object::String(ascii_bytes, StringFormat::Literal);
-                        // Set creator_found to true
-                        creator_found = true;
-                    }
-                    if ascii_key == "Producer" {
-                        // Update the value associated with the key
-                        let ascii_string = "OrangeKiwi using headless Chrome and lopdf";
-                        let ascii_bytes: Vec<u8> = ascii_string.as_bytes().to_vec();
-                        *value = lopdf::Object::String(ascii_bytes, StringFormat::Literal);
-                    }
-                }
-                // If Creator key was found, add/update various PDF properties/meta-data
-                if creator_found {
-                    // Loop through properties set by user
-                    // println!("{}", "PDF document metadata properties".yellow());
-                    for entry in &dictionary_entries {
-                        // println!("* {}: {}", entry.0.cyan(), entry.1.green());
-
-                        let (_key, value) = populate_dictionary(
-                            entry.1.to_string(),
-                            string_values_btreemap.clone(),
-                        );
-                        dictionary.set(entry.0.as_bytes().to_vec(), value);
-                    }
-                }
-
-                object_count += 1;
-            }
-            LopdfObject::Stream(_) => {
-                // It's a stream object
-                object_count += 1;
-            }
-            _ => {
-                // It's some other type of object
+        let mut string_values_btreemap: BTreeMap<String, String> = BTreeMap::new();
+        for (key, value) in yaml_btreemap {
+            if let Value::String(string_value) = value {
+                string_values_btreemap.insert(key, string_value);
             }
         }
-    }
+        let (browser, mut handler) = Browser::launch(BrowserConfig::builder().build()?).await?;
 
-    let check_mark = '\u{2713}';
-    let cross_mark = '\u{2717}';
-    let mut error_message = cross_mark.to_string().red().to_string();
-    error_message.push_str(
-        " Failed to save modified PDF document"
-            .red()
-            .to_string()
-            .as_str(),
-    );
-    doc.save(pdf_file_path.clone())
-        .expect(error_message.as_str());
+        let _handle = async_std::task::spawn(async move {
+            loop {
+                let _event = handler.next().await.unwrap();
+            }
+        });
 
-    println!(
-        "\n{} {} → {}",
-        check_mark,
-        source_file.bright_green(),
-        pdf_file_path
-            .into_os_string()
-            .into_string()
-            .unwrap()
-            .yellow()
-    );
-    println!("{}", "PDF document metadata properties".yellow());
-    for entry in &dictionary_entries {
-        println!("* {}: {}", entry.0.cyan(), entry.1.green());
-    }
+        // let html_string = "<html><head><title>My Title</title><head><body><p>Body p</p></body></html>";
+        let mut html_string = String::new();
+        // Encode the HTML content to URL-safe format
+        // url_escape:: comes from the url_escape crate
+        url_escape::encode_query_to_string(generated_html, &mut html_string);
 
-    Ok(())
+        fs::create_dir_all(&output_directory)?;
+        let mut pdf_file = extracted_filename.unwrap().to_string();
+        pdf_file.push_str(".pdf");
+
+        let pdf_file_path = Path::new(&output_directory).join(pdf_file);
+
+        // Navigate the tab to the HTML content.
+        // In this case, the page is a data stream
+        let page = browser
+            .new_page(format!("data:text/html;charset=utf-8,{}", html_string).as_str())
+            .await?;
+        let _html = page.wait_for_navigation().await?.content().await?;
+        // save the page as pdf
+        // page.save_pdf(PrintToPdfParams::default(), "ok.pdf").await?;
+        // Convert the page to PDF format
+        let pdf = page.pdf(PrintToPdfParams::default()).await?;
+
+        // Create a new PDF document
+        let mut doc: Document = Document::load_mem(&pdf)?;
+        doc.version = pdf_version;
+
+        #[allow(unused_variables)]
+        let mut object_count: i32 = 0;
+        // Iterate over the objects in the PDF document and count them
+        for object_element in &mut doc.objects {
+            let (_key, object) = object_element;
+            match object {
+                LopdfObject::Dictionary(dictionary) => {
+                    // Variable to track if Creator key is present
+                    let mut creator_found = false;
+
+                    // Print out the dictionary entries
+                    for (key, value) in dictionary.iter_mut() {
+                        let ascii_key = String::from_utf8_lossy(key);
+
+                        // Iterate over the key-value pairs in the dictionary
+                        // Check if the key is "Creator"
+                        if ascii_key == "Creator" {
+                            // Update the value associated with the key
+                            let ascii_string = string_values_btreemap.get("generator").unwrap();
+                            let ascii_bytes: Vec<u8> = ascii_string.as_bytes().to_vec();
+                            *value = lopdf::Object::String(ascii_bytes, StringFormat::Literal);
+                            // Set creator_found to true
+                            creator_found = true;
+                        }
+                        if ascii_key == "Producer" {
+                            // Update the value associated with the key
+                            let ascii_string = "OrangeKiwi using headless Chrome and lopdf";
+                            let ascii_bytes: Vec<u8> = ascii_string.as_bytes().to_vec();
+                            *value = lopdf::Object::String(ascii_bytes, StringFormat::Literal);
+                        }
+                    }
+                    // If Creator key was found, add/update various PDF properties/meta-data
+                    if creator_found {
+                        // Loop through properties set by user
+                        // println!("{}", "PDF document metadata properties".yellow());
+                        for entry in &dictionary_entries {
+                            // println!("* {}: {}", entry.0.cyan(), entry.1.green());
+
+                            let (_key, value) = populate_dictionary(
+                                entry.1.to_string(),
+                                string_values_btreemap.clone(),
+                            );
+                            dictionary.set(entry.0.as_bytes().to_vec(), value);
+                        }
+                    }
+
+                    object_count += 1;
+                }
+                LopdfObject::Stream(_) => {
+                    // It's a stream object
+                    object_count += 1;
+                }
+                _ => {
+                    // It's some other type of object
+                }
+            }
+        }
+
+        let check_mark = '\u{2713}';
+        let cross_mark = '\u{2717}';
+        let mut error_message = cross_mark.to_string().red().to_string();
+        error_message.push_str(
+            " Failed to save modified PDF document"
+                .red()
+                .to_string()
+                .as_str(),
+        );
+        doc.save(pdf_file_path.clone())
+            .expect(error_message.as_str());
+
+        println!(
+            "\n{} {} → {}",
+            check_mark,
+            source_file.bright_green(),
+            pdf_file_path
+                .into_os_string()
+                .into_string()
+                .unwrap()
+                .yellow()
+        );
+        println!("{}", "PDF document metadata properties".yellow());
+        for entry in &dictionary_entries {
+            println!("* {}: {}", entry.0.cyan(), entry.1.green());
+        }
+
+        Ok(())
+    })
 }
 
 fn populate_dictionary(
