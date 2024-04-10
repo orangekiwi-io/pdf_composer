9e56a97ce0e64896fd51afe56eb9edce936851ed
orangekiwi-io <hello@orangekiwi.io>
2024-04-10 13:17:50 +0100

    Changed exposure settings of some things

--- src/core.rs
+++ src/core.rs
@@ -4,12 +4,13 @@
 /// The `build_pdf` module contains functions for generating PDF files.
 pub(crate) mod build_pdf;
 /// Re-exports the `build_pdf` function for public use.
-pub use build_pdf::build_pdf;
+pub(crate) use build_pdf::build_pdf;
 
 /// The `page_properties` module contains enums, traits, structs and functions related to page properties (dimensions, margins etc).
 mod page_properties;
+pub(crate) use page_properties::PageMargins;
 /// Re-exports the `PaperSize` and `PaperOrientation` enums for public use.
-pub use page_properties::{PaperSize, PaperOrientation, PageMargins};
+pub use page_properties::{PaperOrientation, PaperSize};
 
 /// The `fonts` module contains enums, traits, structs and functions related to font for PDF documents
 mod fonts;
